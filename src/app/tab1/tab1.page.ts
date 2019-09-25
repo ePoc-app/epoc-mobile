dbe7f2988973c7a6d315375844c081c58975153c
ROSPARS Benoit <benoit.rospars@inria.fr>
2019-09-25 17:27:41 +0200

    Add demo download using file-transfer native plugin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    private progress = 0;
 
-  constructor(public alertController: AlertController) {}
+    constructor(public alertController: AlertController, private file: File, private transfer: FileTransfer) {}
 
     openFile() {
         console.log('open file');
@@ -39,5 +42,30 @@ export class Tab1Page {
         });
 
         await alert.present();
+    }
+
+    demoDownload() {
+        const url = 'https://files.inria.fr/LearningLab_public/LivreNumerique/C026PG-Distrib.zip';
+        const path = 'C026PG-Distrib.zip';
+
+        this.downloadFile(url, path);
+    }
+
+    downloadFile(url: string, fileName: string) {
+        const fileTransfer = this.transfer.create();
+
+        this.progress = 0;
+
+        fileTransfer.download(encodeURI(url), this.file.externalDataDirectory + fileName, true).then((entry) => {
+            console.log('download completed', this.file.externalDataDirectory + fileName);
+        }, (error) => {
+            console.log('download failed: ', error);
+        });
+
+        fileTransfer.onProgress(this.updateProgress);
+    }
+
+    updateProgress(progress) {
+        this.progress = Math.round(progress.loaded / progress.total * 100);
     }
 }
