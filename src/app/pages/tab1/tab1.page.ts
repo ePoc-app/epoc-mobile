3f81bff8b31bd70ef7a61faf2b591cb9cb0b5911
ROSPARS Benoit <benoit.rospars@inria.fr>
2019-12-02 13:53:42 +0100

    Better responsiveness player and library

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Epoc} from '../../classes/epoc';
 import {Reading} from '../../classes/reading';
 import {LibraryService} from '../../services/library.service';
 import {ReadingStoreService} from '../../services/reading-store.service';
+import {IonSlides, Platform} from '@ionic/angular';
 
 @Component({
     selector: 'app-tab1',
@@ -10,23 +11,43 @@ import {ReadingStoreService} from '../../services/reading-store.service';
     styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+
+    @ViewChild('slidesReadings', { static: false }) protected sliderReadings: IonSlides;
+    @ViewChild('slidesLibrary', { static: false }) protected sliderLibrary: IonSlides;
+
     library: Epoc[];
     readings: Reading[];
 
     slidesOptions = {
-        slidesPerView: 2.2
+        slidesPerView: this.platform.width() / 230 // adapt number of slides based on device width
     };
 
     constructor(
+        public platform: Platform,
         private libraryService: LibraryService,
         private readingStore: ReadingStoreService
     ) {
-
+        this.InitPlatformIfReady();
     }
 
     ngOnInit() {
         this.libraryService.getLibrary().subscribe(library => this.library = library);
         this.readingStore.readings$.subscribe(readings => this.readings = readings);
+    }
+
+    InitPlatformIfReady() {
+        this.platform.ready().then(() => {
+            this.platform.resize.subscribe(() => {
+                this.sliderReadings.getSwiper().then((swiper) => {
+                    swiper.params.slidesPerView = this.platform.width() / 230;
+                    swiper.update();
+                });
+                this.sliderLibrary.getSwiper().then((swiper) => {
+                    swiper.params.slidesPerView = this.platform.width() / 230;
+                    swiper.update();
+                });
+            });
+        });
     }
 
     getEpoc(reading) {
