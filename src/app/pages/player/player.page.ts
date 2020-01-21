97b674c6f37e161fb9969f071698228c3d8ba3ff
ROSPARS Benoit <benoit.rospars@inria.fr>
2020-01-21 17:57:51 +0100

    Rewrite reader entirely based on css3 properties

--- src/app/pages/player/player.page.ts
+++ src/app/pages/player/player.page.ts
@@ -1,6 +1,6 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
-import {ActionSheetController, AlertController, IonSlides, Platform} from '@ionic/angular';
+import {AlertController} from '@ionic/angular';
 import {LibraryService} from '../../services/library.service';
 import {switchMap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
@@ -17,41 +17,17 @@ import {Assessment} from '../../classes/contents/assessment';
 })
 export class PlayerPage implements OnInit, AfterViewInit {
 
-    @ViewChild('slides', { static: false }) protected slider: IonSlides;
-
     epoc$: Observable<Epoc>;
 
     readings: Reading[];
 
-    slidesOptions = {
-        slidesPerView: 2,
-        spaceBetween: 0,
-        initialSlide: 0,
-        autoHeight: true,
-        centeredSlides: true,
-        centeredSlidesBounds: true,
-        breakpoints: {
-            600: {
-                slidesPerView: 1.05
-            }
-        }
-    };
-
-    // Styles
-    fontSize = 12;
+    // Reader
+    fontSize = 14;
+    pagePerView = window.innerWidth > 640 ? 2 : 1;
     contentStyles = {'font-size' : this.fontSize + 'px'};
-
-    // Render
-    pagesWrapper = null;
-    rendering = true;
-    splittedContent = [];
     currentPage = 0;
-    pageCount = 0;
-    renderStartTime = 0;
-    renderEndTime = null;
-    currentPageHtml = '';
-    lastInserted = '';
-    openedTags = [];
+    pageCount = 1;
+    pageWrapperTransform = 'translateX(0)';
 
     // Quiz related
     displaySubmit = true;
@@ -76,187 +52,47 @@ export class PlayerPage implements OnInit, AfterViewInit {
                 this.libraryService.getEpoc(params.get('id')))
         );
 
-        this.slidesOptions.initialSlide = +this.route.snapshot.paramMap.get('page');
         this.currentPage = +this.route.snapshot.paramMap.get('page');
     }
 
     ngAfterViewInit() {
-        const player = this;
-        this.epoc$.subscribe({
-            next(epoc) {
-                setTimeout(() => {
-                    player.processContent(epoc.content);
-                }, 200);
-            }
-        });
+        this.initReader();
     }
 
-    processContent(contents) {
-        this.pagesWrapper = this.elRef.nativeElement.querySelector('.pages-wrapper');
-        let htmlContents = '';
-
-        contents.forEach((content) => {
-            if (content.type === 'html') {
-                htmlContents += content.value;
-            }
-        });
-
-        this.splittedContent = htmlContents.match(/<[^>]*>|[^<\s]+/gm);
-        this.renderContent();
+    initReader() {
+        setTimeout(() => {
+            this.pageCount = this.getColumnCount(this.elRef.nativeElement.querySelector('.pages-wrapper'));
+        }, 100);
     }
 
-    resetRender() {
-        this.pagesWrapper.innerHTML = '';
-        this.rendering = true;
-        this.currentPage = 0;
-        this.pageCount = 0;
-        this.renderStartTime = performance.now();
-        this.renderEndTime = null;
-        this.currentPageHtml = '';
-        this.lastInserted = '';
-        this.openedTags = [];
-    }
-
-    renderContent() {
-        this.resetRender();
-
-        const  currentPageContent = this.addPage(null);
-        this.pagesWrapper.appendChild(currentPageContent.parentNode);
-
-        this.fillPage(currentPageContent, this.splittedContent.slice(0));
-    }
-
-    fillPage(currentPageContent, splittedContent) {
-        this.lastInserted = splittedContent.shift();
-
-        // if is opening tag
-        if (this.lastInserted.match(/<[a-z]+.*>/gm)) {
-            this.openedTags.push(this.lastInserted);
-        // if is closing tag
-        } else if (this.lastInserted.match(/<\/[a-z]+.*>/gm)) {
-            this.openedTags.pop();
-        }
-
-        const previousPageHtml = this.currentPageHtml;
-        this.currentPageHtml += this.lastInserted + ' ';
-        currentPageContent.innerHTML = this.currentPageHtml;
-
-        if (this.checkOverflow(currentPageContent)) {
-            splittedContent.unshift(this.lastInserted);
-            currentPageContent.innerHTML = previousPageHtml;
-            this.currentPageHtml = this.openedTags.join();
-            this.openedTags = [];
-            currentPageContent = this.addPage(currentPageContent);
-        }
-
-        if (splittedContent.length > 0) {
-            this.fillPage(currentPageContent, splittedContent);
-        } else {
-            this.renderEndTime = performance.now();
-            this.rendering = false;
-        }
-    }
-
-    addPage(currentPageContent) {
-        const newPageContainer = document.createElement('div');
-        const newPageContent = document.createElement('div');
-
-        this.pageCount++;
-        newPageContainer.className = 'page page-' + this.pageCount;
-        newPageContent.className = 'page-content';
-        newPageContainer.appendChild(newPageContent);
-
-        if (currentPageContent) {
-            currentPageContent.parentNode.after(newPageContainer);
-        }
-
-        return newPageContent;
-    }
-
-    checkOverflow(element) {
-        return element.parentNode.clientHeight < element.clientHeight;
+    getColumnCount(elem) {
+        const lastChild = elem.querySelector('div:last-child');
+        return Math.floor((lastChild.offsetLeft + lastChild.clientWidth) / elem.clientWidth) * this.pagePerView;
     }
 
     changeFontSize(delta) {
-        this.fontSize = this.fontSize + delta;
-        this.contentStyles = {'font-size' : this.fontSize + 'px'};
-        this.rendering = true;
-
-        setTimeout(() => {
-            this.renderContent();
-        }, 1);
-    }
-
-    onSubmit(epoc: Epoc, assessment: Assessment, responses) {
-        const errors = assessment.items.some((question, index) => {
-            return question.correctResponse !== responses[question.type + '-' + index];
-        });
-        const currentReading = this.readingStore.readings.find(reading => reading.epocId === epoc.id);
-
-        if (errors) {
-            this.displayTryagain = true;
-            this.displaySubmit = false;
-        } else {
-            this.displaySubmit = false;
-            this.displayResume = true;
-
-            if (!currentReading.responses[assessment.id]) {
-                this.readingStore.updateScore(epoc.id, assessment.score);
-            }
+        if (this.fontSize + delta > 8 && this.fontSize + delta < 24) {
+            this.fontSize = this.fontSize + delta;
+            this.contentStyles = {'font-size' : this.fontSize + 'px'};
+            this.initReader();
         }
-
-        this.readingStore.saveResponses(epoc.id, assessment.id, responses);
     }
 
-    initSlide(epoc) {
-        const currentReading = this.readingStore.readings.find(reading => reading.epocId === epoc.id);
-        this.isBookmarked = currentReading.bookmarks.indexOf(this.currentPage) !== -1;
+    goToCurrentPage() {
+        this.pageWrapperTransform = 'translateX(-' + this.currentPage * (window.innerWidth / this.pagePerView) + 'px)';
     }
 
-    slideChanged(epoc) {
-        this.slider.getActiveIndex().then((index) => {
-            this.currentPage = index;
-            this.readingStore.updateProgress(epoc.id, (index + 1) / epoc.content.length);
-            const currentReading = this.readingStore.readings.find(reading => reading.epocId === epoc.id);
-            this.isBookmarked = currentReading.bookmarks.indexOf(this.currentPage) !== -1;
-
-            if (epoc.content[this.currentPage].type === 'assessment') {
-                const assessment = epoc.content[this.currentPage];
-                this.displaySubmit = true;
-                if (currentReading.responses[assessment.id]) {
-                    this.onSubmit(epoc, assessment, currentReading.responses[assessment.id]);
-                }
-            }
-        });
+    prevPage() {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+            this.goToCurrentPage();
+        }
     }
 
-    beforeSlideChanged() {
-        this.displaySubmit = false;
-        this.isBookmarked = false;
-        this.displayTryagain = false;
-        this.displayResume = false;
-        this.elRef.nativeElement.querySelectorAll('video').forEach((video) => video.pause());
-    }
-
-    resume() {
-        this.slider.isEnd().then((isEnd) => {
-            if (!isEnd) {
-                this.displaySubmit = true;
-                this.displayResume = false;
-                this.slider.slideNext();
-            }
-        });
-    }
-
-    tryAgain(epoc: Epoc, assessment: Assessment, assessmentForm: NgForm) {
-        this.displaySubmit = true;
-        this.displayTryagain = false;
-        this.readingStore.resetResponses(epoc.id, assessment.id);
-        assessmentForm.reset();
-    }
-
-    toggleBookmark() {
-        this.readingStore.toggleBookmark(this.route.snapshot.paramMap.get('id'), this.currentPage);
-        this.isBookmarked = !this.isBookmarked;
+    nextPage() {
+        if (this.currentPage < this.pageCount) {
+            this.currentPage++;
+            this.goToCurrentPage();
+        }
     }
 }
