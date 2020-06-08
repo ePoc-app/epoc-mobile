7507c99de270f82158f73dfd93bac8393fc67ea1
ROSPARS Benoit <benoit.rospars@inria.fr>
2020-06-08 13:56:25 +0200

    Way to remove dropped item and animation

--- src/app/pages/player/assessment/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/pages/player/assessment/components/drag-and-drop/drag-and-drop.component.ts
@@ -1,10 +1,28 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {trigger, transition, animate, style} from '@angular/animations';
 import {DragAndDropquestion} from '../../../../../classes/contents/assessment';
 
 @Component({
     selector: 'drag-and-drop',
     templateUrl: '../drag-and-drop/drag-and-drop.component.html',
     styleUrls: ['../drag-and-drop/drag-and-drop.component.scss'],
+    animations: [
+        trigger('fadeOut', [
+            transition(':leave', [
+                style({position: 'absolute', width: '100%', top: 0, opacity: 1}),
+                animate('200ms ease-in', style({opacity: 1}))
+            ])
+        ]),
+        trigger('slideInOut', [
+            transition(':enter', [
+                style({opacity: 0, transform: 'translateY(100%)'}),
+                animate('200ms 100ms ease-in', style({opacity: 1, transform: 'translateY(0%)'}))
+            ]),
+            transition(':leave', [
+                animate('200ms ease-in', style({transform: 'translateY(100%)'}))
+            ])
+        ])
+    ]
 })
 export class DragAndDropComponent implements OnInit {
 
@@ -12,10 +30,9 @@ export class DragAndDropComponent implements OnInit {
     @Input('questionMeta') questionMeta: {index: number, total: number};
     @Output() onSelectAnswer = new EventEmitter<any>();
 
+    current
     responses;
-
-    selectedResponse;
-    currentResponseIndex = 0;
+    answer;
 
     constructor() {}
 
@@ -25,36 +42,42 @@ export class DragAndDropComponent implements OnInit {
             .sort((a, b) => a[0] - b[0])
             .map(a => a[1]);
 
-        this.question.responses = shuffleArray(this.question.responses);
+        this.responses = shuffleArray(this.question.responses);
 
-        this.selectedResponse = this.question.responses[0];
-        this.responses = this.question.correctResponse.map((zone) => {
+        this.answer = this.question.correctResponse.map((zone) => {
             return [];
         });
     }
 
-    currentResponse() {
-        return this.question.responses[this.currentResponseIndex];
+    // trick to trigger angular aniamtion
+    getCurrentResponse() {
+        return [this.responses[0]];
     }
 
-    selectZone(index){
-        if (this.currentResponseIndex < this.question.responses.length) {
-            this.responses[index].push(this.selectedResponse);
-            this.currentResponseIndex++;
-            this.selectedResponse = this.currentResponse();
-            if (this.currentResponseIndex === this.question.responses.length) {
+    addResponse(index) {
+        if (this.responses.length > 0) {
+            this.answer[index].push(this.responses.shift());
+            if (this.responses.length === 0) {
                 this.updateAnswer();
             }
         }
+
+        const dropZones = document.querySelectorAll('.drop-zone');
+
+        dropZones.forEach(dropZone => {
+            dropZone.scrollTop = dropZone.scrollHeight - dropZone.clientHeight;
+        });
     }
 
-    selectResponse(response){
-        this.selectedResponse = response;
+    removeResponse($event, zoneIndex, responseIndex) {
+        $event.stopPropagation();
+        const response = this.answer[zoneIndex].splice(responseIndex, 1);
+        this.responses.unshift(...response);
     }
 
     updateAnswer() {
         const answer = [];
-        this.responses.forEach((zone) => {
+        this.answer.forEach((zone) => {
             answer.push(zone.map(response => response.value));
         });
 
