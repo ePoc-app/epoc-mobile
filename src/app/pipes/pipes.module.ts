8389599e4550dff6f5c0ecb02500ceace0771ba5
Benoit Rospars <benoit.rospars@inria.fr>
2020-11-06 15:32:31 +0100

    Create pipe to sanitize local urls

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ValuesPipe } from './values.pipe';
 import { DenormalizePipe } from './denormalize.pipe';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
-    declarations: [ValuesPipe, DenormalizePipe],
-    exports: [ValuesPipe, DenormalizePipe]
+    declarations: [ValuesPipe, DenormalizePipe, SafePipe],
+    exports: [ValuesPipe, DenormalizePipe, SafePipe]
 })
 export class PipesModule {}
