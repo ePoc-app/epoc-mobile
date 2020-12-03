b22defe028f7ca61ade07903532ef9bacc06989d
ROSPARS Benoit <benoit.rospars@inria.fr>
2020-12-03 14:20:29 +0100

    Randomize reorder array

--- src/app/pages/player/assessment/components/reorder/reorder.component.ts
+++ src/app/pages/player/assessment/components/reorder/reorder.component.ts
@@ -17,12 +17,30 @@ export class ReorderComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {
-        this.responses = this.question.responses;
+        this.responses = this.shuffleArray(this.question.responses);
     }
 
     doReorder(ev: any) {
         this.responses = ev.detail.complete(this.responses);
         const answer = this.responses.reduce( (accumulator, response) => accumulator + response.value, '');
         this.onSelectAnswer.emit(answer);
+    }
+
+    shuffleArray(array) {
+        const newArray = [...array]
+        do {
+            for (let i = newArray.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+            }
+        } while (this.arrayEquals(array, newArray));
+        return newArray;
+    }
+
+    arrayEquals(a, b) {
+        return Array.isArray(a) &&
+            Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index]);
     }
 }
