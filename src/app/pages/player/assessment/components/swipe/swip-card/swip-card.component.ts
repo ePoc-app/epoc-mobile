eb96a4762e079c3576dd668f9874a9adfe8a7d4e
gperrin <gerome.perrin@inria.fr>
2021-04-30 10:26:08 +0200

    Issue #143 : Input, Output & données du composant SwipCard

--- src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
+++ src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {SwipCard} from '../../../../../../classes/contents/assessment';
 
 @Component({
   selector: 'app-swip-card',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swip-card.component.scss'],
 })
 export class SwipCardComponent implements OnInit {
+  @Input ('responses') responses: Array<string>;
+  @Input ('SwipCard') swipCard: SwipCard;
+  @Input('disabled') disabled: boolean;
+
+  @Output() onSelectAnswer = new EventEmitter<string>();
+
+  selectedAnswer;
 
   constructor() { }
 
