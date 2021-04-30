3248e0567ef40aa7506b49aa6ff169a6d89f3875
gperrin <gerome.perrin@inria.fr>
2021-04-30 13:51:27 +0200

    Issue #143 : Input, Output & données du composant swipe

--- src/app/pages/player/assessment/components/swipe/swipe.component.ts
+++ src/app/pages/player/assessment/components/swipe/swipe.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {SwipeCard, SwipeQuestion} from '../../../../../classes/contents/assessment';
 
 @Component({
   selector: 'app-swipe',
@@ -7,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwipeComponent implements OnInit {
 
+  @Input ('question') question: SwipeQuestion;
+  @Output() onEndActivity = new EventEmitter<Array<string>>();
+
+  cartesRestantes : Array<SwipeCard>;
+  cartesTriees : Array<SwipeCard>;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.cartesRestantes = this.question.swipeCards;
+  }
 
 }
