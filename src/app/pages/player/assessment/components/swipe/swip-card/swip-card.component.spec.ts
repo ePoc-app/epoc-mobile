3755ed8569e21b10058f2e44688eeee79e60aada
gperrin <gerome.perrin@inria.fr>
2021-04-30 11:38:14 +0200

    Issue #143 : Cas de tests pour selectAnswer(answer)

--- src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.spec.ts
@@ -6,7 +6,6 @@ import { SwipCardComponent } from './swip-card.component';
 describe('SwipCardComponent', () => {
   let component: SwipCardComponent;
   let fixture: ComponentFixture<SwipCardComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SwipCardComponent ],
@@ -21,4 +20,24 @@ describe('SwipCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  // Cas de tests pour la méthode selectAnswer
+  component.responses = ['Vrai', 'Faux'];
+  // Tester que selectedAnswer se met à jour
+  component.selectAnswer('Vrai');
+  it ('should update selectedAnswer', () => {
+    expect(this.selectedAnswer).toBe('Vrai');
+  });
+  // Tester que selectedAnswer ne se met pas à jour si la réponse n'est pas dans responses
+  component.selectAnswer('Impossible')
+  it ('should not update selectedAnswer', () => {
+    expect(Error);
+  })
+  // Tester qu'un évènement onSelectAnswer est déclenché
+  const expectedAnswer = 'Vrai';
+  it ('should emit a onSelectAnswer event to the parent when (swipe)', () => {
+    let selectedAnswer;
+    component.onSelectAnswer.subscribe((answer: string) => selectedAnswer = answer);
+    fixture.debugElement.triggerEventHandler('swipe', null);
+    expect(expectedAnswer).toBe(selectedAnswer);
+  })
 });
--- src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
+++ src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
@@ -19,4 +19,6 @@ export class SwipCardComponent implements OnInit {
 
   ngOnInit() {}
 
+  selectAnswer(answer) {
+  }
 }
