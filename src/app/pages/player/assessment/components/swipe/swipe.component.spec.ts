c36c92675c37a707780461c4c19b70fce732b7ee
gperrin <gerome.perrin@inria.fr>
2021-04-30 14:40:25 +0200

    Issue #143 : Cas de tests pour undo()

--- src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
@@ -2,10 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { SwipeComponent } from './swipe.component';
+import {SwipeCard} from "../../../../../classes/contents/assessment";
 
 describe('SwipeComponent', () => {
   let component: SwipeComponent;
   let fixture: ComponentFixture<SwipeComponent>;
+  const c1 = new SwipeCard();
+  const c2 = new SwipeCard();
+  const c3 = new SwipeCard();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,4 +25,19 @@ describe('SwipeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Cas de tests pour la méthode undo()
+  component.cartesRestantes = [this.c2, this.c3];
+  component.cartesTriees = [this.c1];
+  component.undo();
+  it('should undo', () => {
+    expect(component.cartesTriees).toBe([]);
+    expect(component.cartesRestantes).toBe([this.c1, this.c2, this.c3]);
+  })
+  component.cartesRestantes = [this.c1, this.c2, this.c3];
+  component.cartesTriees = [];
+  component.undo();
+  it('should not undo', () => {
+    expect(Error);
+  })
 });
