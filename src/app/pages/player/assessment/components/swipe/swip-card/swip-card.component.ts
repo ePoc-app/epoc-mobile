002989c820719634c1b4b0c7b35b9ece11327415
gperrin <gerome.perrin@inria.fr>
2021-04-30 13:05:19 +0200

    Issue #143 : Création & implémentation de la gesture 'swipe'

--- src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
+++ src/app/pages/player/assessment/components/swipe/swip-card/swip-card.component.ts
@@ -1,12 +1,21 @@
-import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChildren,
+  AfterViewInit
+} from '@angular/core';
 import {SwipCard} from '../../../../../../classes/contents/assessment';
+import {GestureController, IonCard, Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-swip-card',
   templateUrl: './swip-card.component.html',
   styleUrls: ['./swip-card.component.scss'],
 })
-export class SwipCardComponent implements OnInit {
+export class SwipCardComponent implements AfterViewInit {
   @Input ('responses') responses: Array<string>;
   @Input ('SwipCard') swipCard: SwipCard;
   @Input('disabled') disabled: boolean;
@@ -14,15 +23,42 @@ export class SwipCardComponent implements OnInit {
   @Output() onSelectAnswer = new EventEmitter<string>();
 
   selectedAnswer;
+  @ViewChildren(IonCard, {read:ElementRef}) card: ElementRef;
 
-  constructor() { }
+  constructor(private gestureCtrl: GestureController, private plt: Platform) { }
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    const swipCard = this.card;
+    this.useSwipe(swipCard);
+  }
 
   selectAnswer(answer) {
     if (this.responses.includes(answer)) {
       this.selectedAnswer = answer;
       this.onSelectAnswer.emit(this.selectedAnswer);
     }
+  }
+  useSwipe(card) {
+    const gesture = this.gestureCtrl.create({
+      el: card.nativeElement,
+      gestureName: 'swipe',
+      onStart: ev => {
+
+      },
+      onMove: ev => {
+        card.nativeElement.style.transform = `translateX(${ev.deltaX}px) rotate(${ev.deltaX / 10}deg)`;
+        // Change the title based on the deltaX
+      },
+      onEnd: ev => {
+        card.nativeElement.style.transition = '0.5s ease-out';
+        if (ev.deltaX > 150) {
+          card.nativeElement.style.transform = `translateX(${+this.plt.width() * 2}px) rotate(${ev.deltaX / 2}deg)`;
+        } else if (ev.deltaX < -150) {
+          card.nativeElement.style.transform = `translateX(-${ev.deltaX}px) rotate(${ev.deltaX / 10}deg)`
+        } else {
+          card.nativeElement.style.transform = ``;
+        }
+      }
+    });
   }
 }
