06317e865d03fc0f32f64777e78e6cfbd1772d9e
gperrin <gerome.perrin@inria.fr>
2021-05-03 15:26:41 +0200

    Issue #143 : Tests unitaires fonctionnels

--- src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
@@ -1,24 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
 
 import { SwipeComponent } from './swipe.component';
-import {SwipeCard} from '../../../../../classes/contents/assessment';
+import {SwipeCard, SwipeQuestion} from '../../../../../classes/contents/assessment';
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 
 describe('SwipeComponent', () => {
   let component: SwipeComponent;
   let fixture: ComponentFixture<SwipeComponent>;
-  const c1 = new SwipeCard();
-  const c2 = new SwipeCard();
-  const c3 = new SwipeCard();
+  let c1 = new SwipeCard();
+  let c2 = new SwipeCard();
+  let c3 = new SwipeCard();
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SwipeComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [],
+
     }).compileComponents();
 
     fixture = TestBed.createComponent(SwipeComponent);
     component = fixture.componentInstance;
+    component.question = new SwipeQuestion();
+    c1.id = 1;
+    c1.text = 'Texte à afficher';
+    c2.id = 2;
+    c2.text = 'Deuxième texte';
+    c3.id = 3;
+    c3.text = 'Troisième texte';
     fixture.detectChanges();
   }));
 
@@ -27,17 +36,17 @@ describe('SwipeComponent', () => {
   });
 
   // Cas de tests pour la méthode undo()
-  component.cartesRestantes = [this.c2, this.c3];
-  component.cartesTriees = [this.c1];
-  component.undo();
   it('should undo', () => {
-    expect(component.cartesTriees).toBe([]);
-    expect(component.cartesRestantes).toBe([this.c1, this.c2, this.c3]);
+    component.question.swipeCards = [c1, c2, c3];
+    component.cartesRestantes = [c2, c3];
+    component.cartesTriees = [c1];
+    component.undo();
+    expect(component.cartesTriees).toEqual([]);
+    expect(component.cartesRestantes).toEqual(component.question.swipeCards);
   })
-  component.cartesRestantes = [this.c1, this.c2, this.c3];
-  component.cartesTriees = [];
-  component.undo();
   it('should not undo', () => {
-    expect(Error);
+    component.cartesRestantes = [c1, c2, c3];
+    component.cartesTriees = [];
+    expect(function () { component.undo(); }).toThrow(new Error('Array of cards left is empty'));
   })
 });
