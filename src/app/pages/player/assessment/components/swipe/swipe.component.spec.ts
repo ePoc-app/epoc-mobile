6fa1fbfd80a036f73d6a4875ffd1775a82178625
gperrin <gerome.perrin@inria.fr>
2021-05-03 16:55:59 +0200

    Issue #143 : Tests unitaires méthode onSelectAnswer(answer)

--- src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
@@ -2,7 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SwipeComponent } from './swipe.component';
 import {SwipeCard, SwipeQuestion} from '../../../../../classes/contents/assessment';
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 
 describe('SwipeComponent', () => {
   let component: SwipeComponent;
@@ -28,6 +27,11 @@ describe('SwipeComponent', () => {
     c2.text = 'Deuxième texte';
     c3.id = 3;
     c3.text = 'Troisième texte';
+    component.question.possibilities = ['Vrai','Faux'];
+    component.responses = ['Vrai'];
+    component.question.swipeCards = [c1, c2, c3];
+    component.cartesRestantes = [c2, c3];
+    component.cartesTriees = [c1];
     fixture.detectChanges();
   }));
 
@@ -37,16 +41,36 @@ describe('SwipeComponent', () => {
 
   // Cas de tests pour la méthode undo()
   it('should undo', () => {
-    component.question.swipeCards = [c1, c2, c3];
-    component.cartesRestantes = [c2, c3];
-    component.cartesTriees = [c1];
     component.undo();
     expect(component.cartesTriees).toEqual([]);
     expect(component.cartesRestantes).toEqual(component.question.swipeCards);
-  })
+  });
   it('should not undo', () => {
     component.cartesRestantes = [c1, c2, c3];
     component.cartesTriees = [];
     expect(function () { component.undo(); }).toThrow(new Error('Array of cards left is empty'));
-  })
+  });
+
+  // Cas de tests pour la méthode onSelectAnswer(answer)
+  // Cas classique
+  it ('should push the answer to responses', () => {
+    component.cartesRestantes = [c1, c2, c3];
+    component.onSelectAnswer('Faux');
+    expect(component.responses).toEqual(['Vrai','Faux']);
+  });
+  it ('should emit onEndActivity event', () => {
+    component.cartesRestantes = [];
+    // Vérifier que l'évènement est bien émis
+    spyOn(component.onEndActivity, 'emit');
+    // trigger l'évènement en appelant la méthode
+    component.onSelectAnswer('Faux');
+    expect(component.onEndActivity.emit).toHaveBeenCalledWith(component.responses);
+  });
+  // Cas d'erreur : answer n'est pas une réponse valable
+  it ('should raise an error', () => {
+    expect(function() { component.onSelectAnswer('Impossible');}).toThrow(new Error('Answer is not a possibility'));
+  });
+
 });
+
+
