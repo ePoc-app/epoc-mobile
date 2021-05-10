869089cf338e8968d9f8c6a113c0f34b4bea474c
gperrin <gerome.perrin@inria.fr>
2021-05-10 18:00:55 +0200

    Issue #143 : Fix des tests unitaires

--- src/app/pages/player/assessment/components/swipe/swipe-card/swipe-card.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swipe-card/swipe-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SwipeCardComponent } from './swipe-card.component';
 import {Response} from '../../../../../../classes/contents/assessment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SwipeCardComponent', () => {
   let component: SwipeCardComponent;
@@ -8,12 +9,12 @@ describe('SwipeCardComponent', () => {
   let debugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: []
+      imports: [BrowserAnimationsModule]
     }).compileComponents();
     fixture = TestBed.createComponent(SwipeCardComponent);
     component = fixture.componentInstance;
     debugElement = fixture.debugElement;
-    component.possibilities = ['Vrai','Faux'];
+    component.possibilities = ['Faux', 'Vrai'];
     const rep = new Response();
     rep.label = 'Texte à afficher';
     rep.value = '1';
@@ -29,13 +30,13 @@ describe('SwipeCardComponent', () => {
   // Cas de tests pour la méthode selectSide
   // Tester que selectedAnswer se met à jour
   it ('should update selectedAnswer', () => {
-    component.selectSide('Vrai');
+    component.selectSide(1);
     expect(component.selectedSide).toBe('Vrai');
   });
 
     // Tester que selectedAnswer ne se met pas à jour si la réponse n'est pas dans responses
     it ('should not update selectedAnswer', () => {
-      expect(() => { component.selectSide('Impossible'); }).toThrow(new Error('Answer is not a possibility'));
+      expect(() => { component.selectSide(3); }).toThrow(new Error('Answer is not a possibility'));
       });
 
     // Tester qu'un évènement onSelectSide est déclenché
@@ -43,16 +44,16 @@ describe('SwipeCardComponent', () => {
       spyOn(component.onSelectSide, 'emit');
 
       // trigger l'évènement en appelant la méthode
-      component.selectSide('Faux');
-      expect(component.onSelectSide.emit).toHaveBeenCalledWith({rep: component.response, category:'Faux'});
+      component.selectSide(0);
+      expect(component.onSelectSide.emit).toHaveBeenCalledWith({rep: component.response, category:0});
     })
 
   // Cas de tests pour la méthode displayTitle
-  it ('should update the HTML element with \'\'', () => {
+  it ('should update the HTML element with ?', () => {
     const deltaX = 0;
     const elt = document.getElementsByTagName('ion-card-title')[0];
     component.displayTitle(deltaX, debugElement.nativeElement);
-    expect(elt.innerHTML).toEqual('');
+    expect(elt.innerHTML).toEqual('?');
   })
 
   it ('should update the HTML element with \'Vrai\'', () => {
--- src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
+++ src/app/pages/player/assessment/components/swipe/swipe.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SwipeComponent } from './swipe.component';
 import {Response, SwipeQuestion} from '../../../../../classes/contents/assessment';
+import {ModalController} from '@ionic/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 describe('SwipeComponent', () => {
   let component: SwipeComponent;
@@ -9,11 +12,19 @@ describe('SwipeComponent', () => {
   const c2 = new Response();
   const c3 = new Response();
 
-
+    const modalSpy = jasmine.createSpyObj('Modal', ['present']);
+    const modalCtrlSpy = jasmine.createSpyObj('ModalController', ['create']);
+    modalCtrlSpy.create.and.callFake( () => {
+        return modalSpy;
+    });
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SwipeComponent ],
-      imports: [],
+        providers: [{
+            provide: ModalController,
+            useValue: modalCtrlSpy
+        }],
+      imports: [BrowserAnimationsModule]
 
     }).compileComponents();
 
@@ -28,8 +39,9 @@ describe('SwipeComponent', () => {
     c3.label = 'Troisième texte';
     component.question.possibilities = ['Faux','Vrai'];
     component.question.responses = [c1, c2, c3];
-    component.cartesRestantes = [c2, c3];
-    component.cartesTriees = [c1];
+    component.question.correctResponse = [{label: 'Faux', values:['1', '2']}, {label:'Vrai', values:['3']}];
+    component.cartesRestantes = [c1, c2];
+    component.cartesTriees = [{response: c3, category:0, correct: true}];
     component.cardsToTheLeft = [];
     component.cardsToTheLeft = [];
     fixture.detectChanges();
@@ -41,9 +53,11 @@ describe('SwipeComponent', () => {
 
   // Cas de tests pour la méthode undo()
   it('should undo', () => {
+    component.cartesRestantes = [c1, c2];
+    component.cartesTriees = [{response: c3, category:0, correct: true}];
     component.undo();
     expect(component.cartesTriees).toEqual([]);
-    expect(component.cartesRestantes).toEqual(component.question.responses);
+    expect(component.cartesRestantes).toEqual([c1, c2, c3]);
   });
   it('should not undo', () => {
     component.cartesRestantes = [c1, c2, c3];
@@ -54,23 +68,23 @@ describe('SwipeComponent', () => {
   // Cas de tests pour la méthode onSelectSide(answer)
   // Cas classique
   it ('should push the value of the response to cardsToTheLeft', () => {
-    component.onSelectSide({rep:c1, category:'Faux'});
+    component.onSelectSide({rep:c1, category:0});
     expect(component.cardsToTheLeft).toEqual(['1']);
   });
 
   it ('should emit onSelectAnswer event', () => {
-    component.onSelectSide({rep:c1, category:'Faux'});
-    component.onSelectSide({rep:c2, category:'Faux'});
+    component.onSelectSide({rep:c1, category:0});
+    component.onSelectSide({rep:c2, category:0});
     // Vérifier que l'évènement est bien émis
     spyOn(component.onSelectAnswer, 'emit');
 
     // trigger l'évènement en appelant la méthode
-    component.onSelectSide({rep:c3, category:'Vrai'});
-    expect(component.onSelectAnswer.emit).toHaveBeenCalledWith([{label: 'Faux', values: ['1','2']}, {label: 'Vrai', values: ['3']}]);
+    component.onSelectSide({rep:c3, category:1});
+    expect(component.onSelectAnswer.emit).toHaveBeenCalledWith([['1','2'], ['3']]);
   });
   // Cas d'erreur : answer n'est pas une réponse valable
   it ('should raise an error', () => {
-    expect(() => { component.onSelectSide('Impossible');}).toThrow(new Error('Answer is not a possibility'));
+    expect(() => { component.onSelectSide(3);}).toThrow(new Error('Answer is not a possibility'));
   });
   // Cas : met à jour les deux tableaux cartesTriees, Cartesrestantes
   it ('should update all arrays', () => {
@@ -80,37 +94,41 @@ describe('SwipeComponent', () => {
     component.cardsToTheRight = [];
 
     // Swipe de la première carte qui s'affiche (donc la dernière de l'array) vers la gauche 'catégory : faux'
-    component.onSelectSide({rep:c3, category:'Faux'});
+    component.onSelectSide({rep:c3, category:0});
     expect(component.cartesRestantes).toEqual([c1,c2]);
-    expect(component.cartesTriees).toEqual([c3]);
+    expect(component.cartesTriees).toEqual([{response: c3, category: 0, correct: false}]);
     expect(component.cardsToTheLeft).toEqual([c3.value]);
     expect(component.cardsToTheRight).toEqual([]);
 
     // Swipe de la deuxième carte qui s'affiche (donc la dernière -1 de l'array) vers la droite 'catégory : vrai'
-    component.onSelectSide({rep: c2, category:'Vrai'});
+    component.onSelectSide({rep: c2, category:1});
     expect(component.cartesRestantes).toEqual([c1]);
-    expect(component.cartesTriees).toEqual([c3,c2]);
+    expect(component.cartesTriees).toEqual([{response: c3, category: 0, correct: false}, {response: c2, category: 1, correct:false}]);
     expect(component.cardsToTheLeft).toEqual([c3.value]);
     expect(component.cardsToTheRight).toEqual([c2.value]);
 
-   // Swipe de la troisième carte qui s'affiche (donc la dernière -2 de l'array) vers la droite 'catégory : vrai'
-   component.onSelectSide({rep: c1, category:'Vrai'});
+   // Swipe de la troisième carte qui s'affiche (donc la dernière -2 de l'array) vers la gauche 'catégory : faux'
+   component.onSelectSide({rep: c1, category:0});
    expect(component.cartesRestantes).toEqual([]);
-   expect(component.cartesTriees).toEqual([c3,c2,c1]);
-   expect(component.cardsToTheLeft).toEqual([c3.value]);
-   expect(component.cardsToTheRight).toEqual([c2.value,c1.value]);
+   expect(component.cartesTriees).toEqual([
+       {response: c3, category: 0, correct: false},
+       {response: c2, category: 1, correct:false},
+       {response: c1, category: 0, correct: true}
+   ]);
+   expect(component.cardsToTheLeft).toEqual([c3.value, c1.value]);
+   expect(component.cardsToTheRight).toEqual([c2.value]);
 
    // Appel de la méthode undo
    component.undo();
    expect(component.cartesRestantes).toEqual([c1]);
-   expect(component.cartesTriees).toEqual([c3,c2]);
+      expect(component.cartesTriees).toEqual([{response: c3, category: 0, correct: false}, {response: c2, category: 1, correct:false}]);
    expect(component.cardsToTheLeft).toEqual([c3.value]);
    expect(component.cardsToTheRight).toEqual([c2.value]);
 
    // Appel de la méthode undo
    component.undo();
    expect(component.cartesRestantes).toEqual([c1,c2]);
-   expect(component.cartesTriees).toEqual([c3]);
+      expect(component.cartesTriees).toEqual([{response: c3, category: 0, correct: false}]);
    expect(component.cardsToTheLeft).toEqual([c3.value]);
    expect(component.cardsToTheRight).toEqual([]);
 
