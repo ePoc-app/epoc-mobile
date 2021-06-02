63035d8b55ecf88957c386f1b1228907fcba1295
Benoit Rospars <benoit.rospars@inria.fr>
2021-06-02 14:20:33 +0200

    Issue #151 : fix regex

--- src/app/pipes/src-convert.pipe.ts
+++ src/app/pipes/src-convert.pipe.ts
@@ -1,18 +1,19 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {LibraryService} from '../services/library.service';
 
 @Pipe({
-  name: 'srcConvert'
+    name: 'srcConvert'
 })
 export class SrcConvertPipe implements PipeTransform {
 
-  constructor(
-      public libraryService: LibraryService
-  ) {}
+    constructor(
+        public libraryService: LibraryService
+    ) {
+    }
 
-  transform(value: string): string {
-    const regex = /src='([^']*)'/gm;
-    return value.replace(regex, `src='${this.libraryService.rootFolder}$1'`).replace('assets/demo/', '');
-  }
+    transform(value: string): string {
+        const regex = /src=['"]([^'"]*)['"]/g;
+        return value.replace(regex, `src='${this.libraryService.rootFolder}$1'`).replace(/assets\/demo/g, '');
+    }
 
 }
