e6fdb0e4d0c3b8aa703990537a54261f121ae992
Benoit Rospars <benoit.rospars@inria.fr>
2021-06-02 13:09:58 +0200

    Issue #151 : fix src path in html using pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { ValuesPipe } from './values.pipe';
 import { DenormalizePipe } from './denormalize.pipe';
 import { SafePipe } from './safe.pipe';
+import { SrcConvertPipe } from './src-convert.pipe';
 
 @NgModule({
-    declarations: [ValuesPipe, DenormalizePipe, SafePipe],
-    exports: [ValuesPipe, DenormalizePipe, SafePipe]
+    declarations: [ValuesPipe, DenormalizePipe, SafePipe, SrcConvertPipe],
+    exports: [ValuesPipe, DenormalizePipe, SafePipe, SrcConvertPipe]
 })
 export class PipesModule {}
