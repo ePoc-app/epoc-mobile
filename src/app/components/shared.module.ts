3279de9ddfa40f8797d6655c7093b5d8d7d42f23
ROSPARS Benoit <benoit.rospars@inria.fr>
2021-11-18 11:57:11 +0100

    Remove old library item component

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {IonicModule} from '@ionic/angular';
 import {ScoreProgressComponent} from './score-progress/score-progress.component';
 import {AssessmentButtonComponent} from './assessment-button/assessment-button.component';
-import {LibraryItemComponent} from './libraryItem/library-item.component';
 import {PipesModule} from 'src/app/pipes/pipes.module';
 import {HtmlComponent} from './html/html.component';
 import {RouterModule} from '@angular/router';
@@ -20,14 +19,12 @@ import {VideoPlayerComponent} from './video-player/video-player.component';
     declarations: [
         ScoreProgressComponent,
         AssessmentButtonComponent,
-        LibraryItemComponent,
         HtmlComponent,
         VideoPlayerComponent
     ],
     exports: [
         ScoreProgressComponent,
         AssessmentButtonComponent,
-        LibraryItemComponent,
         HtmlComponent,
         VideoPlayerComponent
     ]
