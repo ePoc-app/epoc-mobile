b0f0713b4f86eecb3b42d173fa823111e48a5092
ROSPARS Benoit <benoit.rospars@inria.fr>
2021-12-06 17:41:35 +0100

    Issue #215 (WIP #176): redesign qcm correction

--- src/app/components/corrections/correction-multiple-choice/correction-multiple-choice.component.ts
+++ src/app/components/corrections/correction-multiple-choice/correction-multiple-choice.component.ts
@@ -8,9 +8,31 @@ import {Question} from 'src/app/classes/contents/assessment';
 })
 export class CorrectionMultipleChoiceComponent implements OnInit {
   @Input() question: Question;
+  @Input() userResponses: any[];
+
+  correction: {correct: boolean, label: string}[] = [];
+  rightResponses = 0;
+  wrongResponses = 0;
+  missingResponses: string[] = [];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.question.responses.forEach((response, index) => {
+      if ((this.question.correctResponse as Array<string>).indexOf(response.value) !== -1) {
+        if (this.userResponses.indexOf(response.value) !== -1) {
+          this.correction.push({correct:true, label: response.label});
+          this.rightResponses++;
+        } else{
+          this.missingResponses.push(response.label)
+        }
+      } else {
+        if (this.userResponses.indexOf(response.value) !== -1) {
+          this.correction.push({correct:false, label: response.label});
+          this.wrongResponses++;
+        }
+      }
+    });
+  }
 
 }
