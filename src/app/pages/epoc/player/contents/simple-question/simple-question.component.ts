3bb560c46fdba23669d85f7d8ce44c5442d16732
ROSPARS Benoit <benoit.rospars@inria.fr>
2021-12-07 11:53:51 +0100

    Issue #219 (WIP #176): redesign drag and drop with gestures

--- src/app/pages/epoc/player/contents/simple-question/simple-question.component.ts
+++ src/app/pages/epoc/player/contents/simple-question/simple-question.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {Question} from 'src/app/classes/contents/assessment';
 import {Reading} from 'src/app/classes/reading';
 import {Content} from 'src/app/classes/contents/content';
@@ -13,6 +13,7 @@ export class SimpleQuestionComponent {
     @Input() content: Content;
     @Input() question: Question;
     @Input() epocId: string;
+    @Output() dragging = new EventEmitter<string>();
 
     @ViewChild(CommonQuestionComponent, { static: false })
     private questionComponent!: CommonQuestionComponent;
@@ -30,5 +31,9 @@ export class SimpleQuestionComponent {
 
     onQuestionAnswered (event) {
         this.questionAnswered = event;
+    }
+
+    onDrag(value){
+        this.dragging.emit(value);
     }
 }
