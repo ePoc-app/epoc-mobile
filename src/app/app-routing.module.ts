b2d33f5c6cfeb258f649cd831d84ed26984d1d8b
Benoit Rospars <benoit.rospars@inria.fr>
2022-01-28 16:49:31 +0100

    Set npm run script and reactivate auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routesDefault: Routes = [
 const routesInria: Routes = [
     {
         path: 'epoc',
-        // canActivate: [AuthGuardService],
+        canActivate: [AuthGuardService],
         loadChildren: () => import('./pages/epoc/epoc.module').then(m => m.EpocModule)
     },
     {
