a9865e384d723ad38cf0ba798d6f31412ff7a4df
Benoit Rospars <benoit.rospars@inria.fr>
2022-11-28 17:35:01 +0100

    Issue #336 : improve mode script to change package id

--- scripts/build-before.js
+++ scripts/build-before.js
@@ -5,9 +5,30 @@ module.exports = function(ctx) {
   const fileContent = `// This file is replaced during build by using --mode=ill|inria|normal
 export const mode:'ill'|'inria'|'normal' = '${mode}';
 `;
+
+  console.log(`MODE : ${mode}`)
   
   fs.writeFile('src/environments/environment.mode.ts', fileContent, function (err) {
     if (err) return console.log(err);
-    console.log(`Mode : ${mode}`)
   });
+
+  const packageIdAndroid = `fr.inria.epoc`;
+  const packageIdApple = `fr.inria.learninglab.epoc`;
+
+  console.log(`ANDROID PACKAGE ID : ${packageIdAndroid}${mode !== 'normal' ? '.zrr' : ''}`)
+  console.log(`APPLE PACKAGE ID : ${packageIdAndroid}${mode !== 'normal' ? '.zrr' : ''}`)
+
+  searchAndReplace('android/app/build.gradle', new RegExp(`${packageIdAndroid}${mode !== 'normal' ? '': '.zrr'}`,'g'), `${packageIdAndroid}${mode !== 'normal' ? '.zrr' : ''}`)
+  searchAndReplace('ios/App/App.xcodeproj/project.pbxproj', new RegExp(`${packageIdApple}${mode !== 'normal' ? '': '.zrr'}`,'g'), `${packageIdApple}${mode !== 'normal' ? '.zrr' : ''}`)
 };
+
+function searchAndReplace(file, search, replace) {
+  fs.readFile(file, 'utf8', function (err,data) {
+    if (err) return console.log(err);
+    const result = data.replace(search, replace);
+
+    fs.writeFile(file, result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
+  });
+}
