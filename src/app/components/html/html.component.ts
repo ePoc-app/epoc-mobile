cf5ddd36395e1256c571769f38ea4a6c32f6723b
VIAUD Nathan <nathan.viaud@inria.fr>
2024-02-29 11:31:38 +0100

    feat: add support for mathjax

--- src/app/components/html/html.component.ts
+++ src/app/components/html/html.component.ts
@@ -11,8 +11,8 @@ import {
     ViewEncapsulation
 } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {EpocService} from '../../services/epoc.service';
 import {PluginService} from 'src/app/services/plugin.service';
+import renderMathInElement from 'katex/contrib/auto-render';
 import mermaid from 'mermaid';
 
 @Component({
@@ -57,16 +57,35 @@ export class HtmlComponent implements OnInit, OnDestroy {
 
     // tslint:disable-next-line:use-lifecycle-interface
     async ngAfterViewInit() {
+        this.renderMath()
+
+        await this.renderMermaid()
+    }
+
+    private async renderMermaid() {
         mermaid.initialize({ startOnLoad: false });
         const mermaidDiagram = this.content.nativeElement.querySelector('.mermaid');
         if(mermaidDiagram) {
             const { svg } = await mermaid.render('mermaid-svg', mermaidDiagram.innerHTML);
             mermaidDiagram.innerHTML = svg;
         }
+
+    }
+
+    private renderMath() {
+        renderMathInElement(this.content.nativeElement, {
+            delimiters: [
+                { left: '$$', right: '$$', display: true },
+                { left: '$', right: '$', display: false },
+                { left: '\\(', right: '\\)', display: false },
+                { left: '\\[', right: '\\]', display: true },
+            ],
+            throwOnError: false,
+            output: 'html'
+        });
     }
 
     ngOnDestroy() {
         this.clickListener();
     }
-
 }
