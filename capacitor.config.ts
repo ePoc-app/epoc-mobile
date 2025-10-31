bba18f69cf037bd535782a6780fd83953cde21fc
Benoit Rospars <benoit.rospars@inria.fr>
2025-10-31 15:19:50 +0100

    chores: remove old native dependencies and fix config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fr.inria.epoc',
   appName: 'ePoc',
-  bundledWebRuntime: false,
-  webDir : 'www',
+  webDir : 'dist',
 }
 
+export default config;
