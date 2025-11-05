22799d609a765c7fb67ad566e3e270ab5ba898dd
edelagnier <erica.delagnier@multi.coop>
2025-11-05 15:29:59 +0100

    add icons

--- src/views/library/LibraryPage.vue
+++ src/views/library/LibraryPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonButton } from '@ionic/vue';
-  import { settingsOutline, informationCircleOutline } from 'ionicons/icons';
+  import { settingsOutline, informationCircleOutline, cloudDownloadOutline, chevronForwardOutline,arrowForwardOutline, cogOutline, syncOutline} from 'ionicons/icons';
   import {useLibraryStore} from '@/stores/libraryStore';
   import { RouterLink } from 'vue-router';
 
@@ -42,7 +42,7 @@
           <div class="library-items" tabindex="-1">
             <div class="library-header">
               <span>{{collection.title}}</span>
-              <RouterLink :to="collection.id">{{$t('LIBRARY_PAGE.VIEW_ALL')}} <ion-icon name="chevron-forward-outline"></ion-icon></RouterLink>
+              <RouterLink :to="collection.id">{{$t('LIBRARY_PAGE.VIEW_ALL')}} <ion-icon :icon="chevronForwardOutline"></ion-icon></RouterLink>
             </div>
             <div class="library-item" v-for="epoc in Object.values(collection.ePocs).slice(0,4)" :key="epoc.id">
               <div role="link" :aria-label="epoc.title" class="library-item-image" :routerLink="'/library/'+collection.id+'/'+epoc.id" :style="'background-image:url('+epoc.image+')'"></div>
@@ -50,7 +50,7 @@
               <div class="library-item-toolbar" v-if="epoc.downloaded">
                 <ion-button  class="expanded" color="inria" :routerLink="'/epoc/toc/'+epoc.id">
                   <span v-if="epoc.opened">{{$t('LIBRARY_PAGE.CONTINUE')}}</span>
-                  <ion-icon aria-hidden="true" v-if="epoc.opened" name="arrow-forward-outline" slot="end"></ion-icon>
+                  <ion-icon aria-hidden="true" v-if="epoc.opened" :icon="arrowForwardOutline" slot="end"></ion-icon>
                   <span v-if="!epoc.opened">{{$t('LIBRARY_PAGE.DISCOVER')}}</span>
                 </ion-button>
                 <ion-button class="round" :class="{'update-available': epoc.updateAvailable}" color="inria-base-button" v-on:click="libraryStore.epocLibraryMenu(epoc, collection.id)">
@@ -59,13 +59,13 @@
               </div>
               <div class="library-item-toolbar" v-if="!epoc.downloading && !epoc.downloaded && !epoc.unzipping">
                 <ion-button class="expanded" color="inria-base-button" v-on:click="libraryStore.downloadEpoc(epoc, collection.id)">
-                  <ion-icon aria-hidden="true" name="cloud-download-outline" slot="start"></ion-icon>
+                  <ion-icon aria-hidden="true" :icon="cloudDownloadOutline" slot="start"></ion-icon>
                   <span class="base-btn">{{$t('LIBRARY_PAGE.DOWNLOAD')}}</span>
                 </ion-button>
               </div>
               <div class="library-item-toolbar" v-if="epoc.downloading">
-                <ion-button class="expanded" disabled="true" color="inria-base-button">
-                  <ion-icon aria-hidden="true" name="sync-outline" class="spin" slot="start"></ion-icon>
+                <ion-button class="expanded" :disabled="true" color="inria-base-button">
+                  <ion-icon aria-hidden="true" :icon="syncOutline" class="spin" slot="start"></ion-icon>
                   <span class="base-btn">
                     {{$t('LIBRARY_PAGE.DOWNLOADING')}} 
                     <!--<template v-if="epocProgresses[epoc.id]">({{epocProgresses[epoc.id]}}%)</template>-->
@@ -73,8 +73,8 @@
                 </ion-button>
               </div>
               <div class="library-item-toolbar" v-if="epoc.unzipping">
-                <ion-button class="expanded" disabled="true" color="inria-base-button">
-                  <ion-icon aria-hidden="true" name="cog-outline" class="spin" slot="start"></ion-icon>
+                <ion-button class="expanded" :disabled="true" color="inria-base-button">
+                  <ion-icon aria-hidden="true" :icon="cogOutline" class="spin" slot="start"></ion-icon>
                   <span class="base-btn">
                     {{$t('LIBRARY_PAGE.OPEN_ZIP')}}
                     <!--<template v-if="epocProgresses[epoc.id]">({{epocProgresses[epoc.id]}}%)</template>-->
@@ -83,7 +83,7 @@
               </div>
             </div>
             <div class="library-footer" v-if="Object.keys(collection.ePocs).length > 4">
-              <RouterLink :to="collection.id">{{$t('LIBRARY_PAGE.VIEW_ALL')}} <ion-icon name="chevron-forward-outline"></ion-icon></RouterLink>
+              <RouterLink :to="collection.id">{{$t('LIBRARY_PAGE.VIEW_ALL')}} <ion-icon :icon="chevronForwardOutline"></ion-icon></RouterLink>
             </div>
           </div>
         </div>
