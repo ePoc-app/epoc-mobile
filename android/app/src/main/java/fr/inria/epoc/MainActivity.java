7c9bfba644d7babbed0479f4ffe842d56528c74f
Benoit Rospars <benoit.rospars@inria.fr>
2025-11-07 09:58:11 +0100

    refactor: upgrade android config

--- android/app/src/main/java/fr/inria/epoc/MainActivity.java
+++ android/app/src/main/java/fr/inria/epoc/MainActivity.java
@@ -1,18 +1,5 @@
 package fr.inria.epoc;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import co.fitcom.capacitor.ZipPlugin;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(ZipPlugin.class);
-    super.onCreate(savedInstanceState);
-
-  }
-}
+public class MainActivity extends BridgeActivity {}
