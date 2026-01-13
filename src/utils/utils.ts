511f5a6499454973d1e5d3d2842b88a38cff0d30
edelagnier <erica.delagnier@multi.coop>
2026-01-13 18:46:12 +0100

    translation draft

---
+++ src/utils/utils.ts
@@ -0,0 +1,9 @@
+export const shuffleArray = (arr : any[] ): any[] => { 
+    return arr.map(a => [Math.random(), a])
+            .sort((a, b) => a[0] - b[0])
+            .map(a => a[1])
+    };
+
+export const clamp = (min: number, n: number, max: number) => {
+      return Math.max(min, Math.min(n, max));
+    };
