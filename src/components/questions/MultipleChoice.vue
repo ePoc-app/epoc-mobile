fdb44b3fded18f615134a5925fe4804df9bf1cd4
edelagnier <erica.delagnier@multi.coop>
2026-01-15 12:21:11 +0100

    feat: multiple choice

--- src/components/questions/MultipleChoice.vue
+++ src/components/questions/MultipleChoice.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
-// TO DO
+import { MultipleChoiceQuestion } from '@/types/contents/assessment';
+import { PropType, ref } from 'vue';
+import { IonItem, IonCheckbox, IonLabel, IonList } from '@ionic/vue';
+
+const props = defineProps({
+    question: { type : Object as PropType<(MultipleChoiceQuestion)>, required: true},
+    disabled: Boolean,
+    userPreviousResponse: Array<String>,
+})
+
+const emits = defineEmits<{
+  userHasResponded: [userResponses: any[]]; 
+}>()
+
+const selectedAnswers = ref<any[]>((props.userPreviousResponse && props.userPreviousResponse.length > 0) ? props.userPreviousResponse : []);
+
+const selectAnswer = (answer: any) => {
+    const index = selectedAnswers.value.indexOf(answer.detail.value);
+    if (answer.detail.checked && index === -1) {
+        selectedAnswers.value.push(answer.detail.value);
+    } else {
+        if (index >= 0) {
+            selectedAnswers.value.splice(index, 1);
+        }
+    }
+    emits('userHasResponded', selectedAnswers.value);
+}
 </script>
 
 <template>
-MULTIPLE CHOICE
+<ion-list>
+  <ion-item v-for="response of question.responses" lines="none">
+    <ion-checkbox mode="md" slot="start" :value="response.value" @ionChange="selectAnswer"
+                  :checked="selectedAnswers.indexOf(response.value) !== -1" :disabled="disabled">
+    </ion-checkbox>
+    <ion-label class="ion-text-wrap">{{response.label}}</ion-label>
+  </ion-item>
+</ion-list>
 </template>
 
 <style>
